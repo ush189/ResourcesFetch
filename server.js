ec4cdbada7b9e56ef28a795ddbe5e9fc5d07f0b9
ush189 <github@ush189.de>
2015-05-15 23:44:02 +0200

    implemented request to resources price list

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express = require('express');
+var request = require('request');
+
 var app = express();
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
+});
+
+app.get('/prices', function (req, res) {
+    request('http://www.resources-game.ch/exchange/kurseliste_json.txt', function(error, response, body) {
+        if (error) {
+            res.send({});
+        } else {
+            res.send(JSON.parse(body));
+        }
+    });
 });
 
 app.listen(3000, function () {
